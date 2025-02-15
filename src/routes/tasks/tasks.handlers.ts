24c1e908da8e74a3faeead56075e65ecb45087ed
impruthvi <pruthvirajput97@gmail.com>
2025-02-15 14:38:54 +0530

    feat: add get one task handler and route, include not found error handling

--- src/routes/tasks/tasks.handlers.ts
+++ src/routes/tasks/tasks.handlers.ts
@@ -1,7 +1,8 @@
 import * as HttpStatusCodes from "stoker/http-status-codes";
+import * as HttpStatusPhrases from "stoker/http-status-phrases";
 
 import type { AppRouteHandler } from "@/lib/types";
-import type { CreateRoute, ListRoute } from "@/routes/tasks/tasks.routes";
+import type { CreateRoute, GetOneRoute, ListRoute } from "@/routes/tasks/tasks.routes";
 
 import db from "@/db";
 import { tasks } from "@/db/schema";
@@ -15,4 +16,24 @@ export const create: AppRouteHandler<CreateRoute> = async (c) => {
   const task = c.req.valid("json");
   const [inserted] = await db.insert(tasks).values(task).returning();
   return c.json(inserted, HttpStatusCodes.CREATED);
+};
+
+export const getOne: AppRouteHandler<GetOneRoute> = async (c) => {
+  const { id } = c.req.valid("param");
+  const task = await db.query.tasks.findFirst({
+    where(fields, operators) {
+      return operators.eq(fields.id, id);
+    },
+  });
+
+  if (!task) {
+    return c.json(
+      {
+        message: HttpStatusPhrases.NOT_FOUND,
+      },
+      HttpStatusCodes.NOT_FOUND,
+    );
+  }
+
+  return c.json(task, HttpStatusCodes.OK);
 };
