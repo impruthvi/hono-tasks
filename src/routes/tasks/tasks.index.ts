c4f43d3233f5ef6189c3be54ae8952ade945d683
impruthvi <pruthvirajput97@gmail.com>
2025-02-16 21:29:53 +0530

    refactor: remove redundant export statement in tasks router

--- src/routes/tasks/tasks.index.ts
+++ src/routes/tasks/tasks.index.ts
@@ -8,11 +8,10 @@ const router = createRouter()
 
 router.use('/*', createAuthMiddleware());
 
-router
+export default router
   .openapi(routes.list, handlers.list)
   .openapi(routes.create, handlers.create)
   .openapi(routes.getOne, handlers.getOne)
   .openapi(routes.patch, handlers.patch)
   .openapi(routes.remove, handlers.remove);
 
-export default router;
